6d85c2dcc2a286c8a0885703979822f17b65c9e8
esohkevin <esohkevin4@gmail.com>
2019-01-27 20:09:05 +0300

    add vcfcheck.sh

---
+++ vcfcheck.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+read -p "Is each chromosome in a separate file? [yes|no] : " res
+
+if [[ "$res" == [yY] || "$res" == "yes" || "$res" == "YES" ]];
+then
+	echo -e "\n\e[30;1mUsage: checkVCF.py ref-file vcf-file\e[30;1m\n"
+        read -p """Enter the reference sequence first, then the vcf file separated by a space
+                         (Please provide the absolute path to the files if you are not currently in the containing directory) 
+                         Enter the basename of your vcf file e.g. camgwas-cr1.vcf.gz; basename = camgwas-chr
+"""
+        ref="$1"
+        vcfbase="$2"
+        for i in $(seq 1 22);
+        do
+                echo "`checkVCF.py -r "$ref" -o out "${vcfbase}""${i}".vcf.gz`"
+        done
+else
+	echo -e "\n\e[30;1mUsage: checkVCF.py ref-file vcf-file\e[30;1m\n"
+        read -p """Enter the reference sequence first, then the vcf file separated by a space
+"""
+        echo "`checkVCF.py -r "$ref" -o out "${vcfbase}".vcf.gz`"
+fi
+
