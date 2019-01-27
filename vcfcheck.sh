b625effeeca21ccef0d913408be8e89000b3d305
esohkevin <esohkevin4@gmail.com>
2019-01-27 17:54:59 +0000

    add vcfcheck.sh

--- vcfcheck.sh
+++ vcfcheck.sh
@@ -4,11 +4,12 @@ read -p "Is each chromosome in a separate file? [yes|no] : " res
 
 if [[ "$res" == [yY] || "$res" == "yes" || "$res" == "YES" ]];
 then
-	echo -e "\n\e[30;1mUsage: checkVCF.py ref-file vcf-file\e[30;1m\n"
+	echo -e "\e[32mUsage: checkVCF.py ref-file vcf-file\e[32m";
+	echo " "
         read -p """Enter the reference sequence first, then the vcf file separated by a space
-                         (Please provide the absolute path to the files if you are not currently in the containing directory) 
-                         Enter the basename of your vcf file e.g. camgwas-cr1.vcf.gz; basename = camgwas-chr
-"""
+                   (Please provide the absolute path to the files if you are not currently in the containing directory) 
+                   Enter the basename of your vcf file e.g. camgwas-cr1.vcf.gz; basename = camgwas-chr
+: """
         ref="$1"
         vcfbase="$2"
         for i in $(seq 1 22);
@@ -16,9 +17,12 @@ then
                 echo "`checkVCF.py -r "$ref" -o out "${vcfbase}""${i}".vcf.gz`"
         done
 else
-	echo -e "\n\e[30;1mUsage: checkVCF.py ref-file vcf-file\e[30;1m\n"
-        read -p """Enter the reference sequence first, then the vcf file separated by a space
-"""
-        echo "`checkVCF.py -r "$ref" -o out "${vcfbase}".vcf.gz`"
+	echo -e "\n\e[30;1mUsage: checkVCF.py ref-file vcf-file\e[30;1m\n";
+        echo " "
+	read -p """Enter the reference sequence first, then the vcf file separated by a space
+: """
+        ref="$1"
+        vcfbase="$2"
+	echo "`checkVCF.py -r "$ref" -o out "${vcfbase}".vcf.gz`"
 fi
 
