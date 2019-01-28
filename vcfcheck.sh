cde5bc87bb3708e3605bf59108f848f37f09d0aa
esohkevin <esohkevin4@gmail.com>
2019-01-28 14:30:58 +0300

    update

--- vcfcheck.sh
+++ vcfcheck.sh
@@ -8,12 +8,16 @@ then
         vcfbase="qc-camgwas-updated-chr"
         for i in $(seq 1 22);
         do
+		tabix -p vcf "${vcfbase}""${i}".vcf.gz
+		bcftools sort "${vcfbase}""${i}".vcf.gz -Oz -o "${vcfbase}""${i}".vcf.gz
                 echo "`checkVCF.py -r "$ref" -o out "${vcfbase}""${i}".vcf.gz`"
         done
 elif [[ "$res" == "no" ]];
 then
 	ref="human_g1k_v37.fasta"
         vcfbase="qc-camgwas-updated"
+	tabix -p vcf "${vcfbase}".vcf.gz
+	bcftools sort "${vcfbase}".vcf.gz -Oz -o "${vcfbase}".vcf.gz
 	echo "`checkVCF.py -r "$ref" -o out "${vcfbase}".vcf.gz`"
 fi
 
