8eee047c9de06b17b9aa91ffb9657e85e1e4c982
esohkevin <esohkevin4@gmail.com>
2019-02-02 07:19:17 +0300

    moved files into more specialized directories

--- xsnpmissing.R
+++
@@ -1,5 +0,0 @@
-# X chromosome differential missingness
-Xdiffmiss=read.table("qc-camgwas-chrX.missing", header = T, as.is = T)
-Xdiffmiss=Xdiffmiss[Xdiffmiss$P<0.000001, ]
-write.table(Xdiffmiss$SNP, file = "fail-Xdiffmiss.qc", row.names = F, col.names = F, quote = F)
-~                                                                                               
