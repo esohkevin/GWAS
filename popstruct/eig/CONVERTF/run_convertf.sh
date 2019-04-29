f867c970a5bdf597fc97693e914f943f2b0d6853
esohkevin <esohkevin4@gmail.com>
2019-04-29 16:00:38 +0000

    update

--- popstruct/eig/CONVERTF/run_convertf.sh
+++ popstruct/eig/CONVERTF/run_convertf.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Prune the qc-dataset for SNPs within 50bp with r^2 < 0.2 using a window of 5 SNPs
+plink \
+        --bfile ../../../analysis/qc-camgwas-updated \
+        --indep-pairwise 50 5 0.2 \
+        --allow-no-sex \
+        --autosome \
+        --biallelic-only \
+        --out qc-ldPruned
+
+plink \
+        --bfile ../../../analysis/qc-camgwas-updated \
+        --extract qc-ldPruned.prune.in \
+        --allow-no-sex \
+        --autosome \
+        --make-bed \
+        --out qc-camgwas-ldPruned
+
+plink \
+	--bfile qc-camgwas-ldPruned \
+	--recode \
+	--allow-no-sex \
+	--keep-allele-order \
+	--double-id \
+	--out qc-camgwas
+
 # Convert files into eigensoft compartible formats
 convertf -p par.PED.PACKEDPED 	# ped to packedped
 convertf -p par.PACKEDPED.PACKEDANCESTRYMAP	# packedped to packedancestrymap
--- popstruct/eig/POPGEN/run_popgenstats.sh
+++ popstruct/eig/POPGEN/run_popgenstats.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 smartpca -p par.smartpca-fst > smartpca-fst.log
 smartpca -p par.smartpca-pca-grm > smartpca-pca-grm.log
