a2307db0eead08155a4c6da0923a65196049f076
esohkevin <esohkevin4@gmail.com>
2019-04-30 12:06:06 +0000

    update

---
+++ michigan/shapeit/post-imput-assoc.R
@@ -0,0 +1,82 @@
+#!/usr/bin/Rscript
+
+#################### Association Test Accounting for Population Structure #########################
+if (!requireNamespace("qqman")) 
+	install.packages("qqman", repos="http://cloud.r-project.org", ask = F)
+library(qqman)
+
+### Now read in association test data after including axes of genetic variation as covariats
+# Using PC1 and PC2
+psassoc=read.table("post-imput.assoc.logistic", header = T, as.is = T)
+
+# Now assess the genomic control inflation factor
+median(qchisq(psassoc[,9], df=1, lower.tail = F), na.rm = T)/0.456  # 1.077156
+
+# Now produce association plot
+snpsofinterest=assoc2[-log10(assoc2$P)>=7,]
+png("im-assoc_qc.png", res = 1200, height =3, width = 7, units = "in")
+par(mfrow=c(1,1), cex=0.5, mai=c(0.5,0.3,0.5,0.3))
+manhattan(psassoc, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"), chrlabs = c(1:22),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpofinterest, logp = T,
+          annotatePval = 1e-07, annotateTop = T, ylim = c(0, 9), cex = 0.6, cex.axis = 0.9)
+dev.off()
+
+# Using PC1, PC5 and PC9
+psassoc1=read.table("post-impc1c2.assoc.logistic", header = T, as.is = T)
+
+# Now assess the genomic control inflation factor
+median(qchisq(psassoc1[,9], df=1, lower.tail = F), na.rm = T)/0.456 # 1.061969
+
+# Now produce association plot
+snpsofinterest=assoc2[-log10(assoc2$P)>=7,]
+png("im1-assoc_qc.png", res = 1200, height = 3, width = 7, units = "in")
+par(mfrow=c(1,1), cex=0.5, mai=c(0.5,0.3,0.5,0.3))
+manhattan(psassoc1, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"), chrlabs = c(1:22),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpofinterest, logp = T,
+          annotatePval = 1e-07, annotateTop = T, ylim = c(0, 9), cex = 0.6, cex.axis = 0.9)
+dev.off()
+
+# Using all PCs
+psassoc2=read.table("post-impc1c5c9.assoc.logistic", header = T, as.is = T)
+
+# Now assess the genomic control inflation factor
+median(qchisq(psassoc2[,9], df=1, lower.tail = F), na.rm = T)/0.456
+
+# Now produce association plot
+snpsofinterest=assoc2[-log10(assoc2$P)>=7,]
+png("im2-assoc_qc.png", res = 1200, height = 3, width = 7, units = "in")
+par(mfrow=c(1,1), cex=0.5, mai=c(0.5,0.3,0.5,0.3))
+manhattan(psassoc2, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"), chrlabs = c(1:22),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpofinterest, logp = T,
+          annotatePval = 1e-07, annotateTop = T, ylim = c(0, 9), cex = 0.6, cex.axis = 0.9)
+dev.off()
+
+# Run Association for imputed dataset using additive MOI
+psassoc3=read.table("post-impc1-c10.assoc.logistic", header = T, as.is = T)
+png("im-add-assoc_qc.png", res = 1200, height = 3, width = 6, units = "in")
+par(mfrow=c(1,1), cex=0.5, mai=c(0.5,0.3,0.5,0.3))
+manhattan(psassoc3, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"), chrlabs = c(1:22),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpofinterest, logp = T,
+          annotatePval = 1e-07, annotateTop = T, ylim = c(0, 9), cex = 0.6, cex.axis = 0.9)
+dev.off()
+
+
+# Run Association for imputed dataset using hethom MOI
+psassoc4=read.table("post-impc1-c10-hethom-noNA.assoc.logistic", header = T, as.is = T)
+png("im-hethom-assoc_qc.png", res = 1200, height = 3, width = 6, units = "in")
+par(mfrow=c(1,1), cex=0.5, mai=c(0.5,0.3,0.5,0.3))
+manhattan(psassoc4, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"), chrlabs = c(1:22),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpofinterest, logp = T,
+          annotatePval = 1e-07, annotateTop = T, cex = 0.6, cex.axis = 0.9)
+dev.off()
+
+
+# Plot a Q-Q plot for the association analysis
+png("qq_plots.png", res=1200, height=7, width=7, units="in")
+par(mfrow=c(2,2), cex=0.6)
+qq(psassoc$P, main="Q-Q plot after QC")
+qq(psassoc1$P, main="Q-Q plot after QC")
+qq(psassoc2$P, main="Q-Q plot after QC")
+dev.off()
+
+
