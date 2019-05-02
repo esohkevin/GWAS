fbdfce4ca76f9e75b916b0aedf8d6557d4051cbc
esohkevin <esohkevin4@gmail.com>
2019-05-03 01:00:55 +0300

    update

--- michigan/shapeit/post-imput-assoc.R
+++ michigan/shapeit/post-imput-assoc.R
@@ -5,78 +5,100 @@ if (!requireNamespace("qqman"))
 	install.packages("qqman", repos="http://cloud.r-project.org", ask = F)
 library(qqman)
 
-### Now read in association test data after including axes of genetic variation as covariats
-# Using PC1 and PC2
-psassoc=read.table("post-imput.assoc.logistic", header = T, as.is = T)
+### Now read in assoc data of imputed set after including axes of genetic variation as covariats
+###################### Using PC1 and PC2 ##############################
+imassoc=read.table("post-impc1c2.assoc.logistic", header = T, as.is = T)
 
 # Now assess the genomic control inflation factor
-median(qchisq(psassoc[,9], df=1, lower.tail = F), na.rm = T)/0.456  # 1.077156
+median(qchisq(imassoc[,9], df=1, lower.tail = F), na.rm = T)/0.456  
 
 # Now produce association plot
-#snpsofinterest=assoc2[-log10(assoc2$P)>=7,]
-png("im-assoc_qc.png", res = 1200, height =3, width = 6, units = "in")
-par(mfrow=c(1,1), cex=0.7, mai=c(0.5,0.3,0.5,0.3))
-manhattan(psassoc, chr = "CHR", bp = "BP", p = "P", col = c("gray10", "gray60"), chrlabs = NULL,
-          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = NULL, logp = T,
-          annotatePval = NULL, annotateTop = T)
+snpsofinterest=imassoc[-log10(imassoc$P)>=7,]
+#png("im-assoc_qc.png", res = 1200, height =3, width = 6, units = "in")
+png(filename = "plink-imp-assocC1C2.png", width = 780, height = 480, units = "px", pointsize = 12, 
+    bg = "white",  res = NA)
+par(mfrow=c(1,1))
+manhattan(fall, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"), 
+	  suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpsofinterest$SNP, logp = T,
+	  annotatePval = 0.0000001, annotateTop = T, ylim = c(0, 9))
 dev.off()
 
-# Using PC1, PC5 and PC9
-psassoc1=read.table("post-impc1c2.assoc.logistic", header = T, as.is = T)
+############################## Using PC1, PC5 and PC9 ################################
+imassoc1=read.table("post-impc1c5c9.assoc.logistic", header = T, as.is = T)
 
 # Now assess the genomic control inflation factor
-median(qchisq(psassoc1[,9], df=1, lower.tail = F), na.rm = T)/0.456 # 1.061969
+median(qchisq(imassoc1[,9], df=1, lower.tail = F), na.rm = T)/0.456
 
 # Now produce association plot
-#snpsofinterest=assoc2[-log10(assoc2$P)>=7,]
-png("im1-assoc_qc.png", res = 1200, height = 5, width = 9, units = "in")
+snpsofinterest=imassoc1[-log10(imassoc1$P)>=7,]
+#png("im-assoc_qc.png", res = 1200, height =3, width = 6, units = "in")
+png(filename = "plink-imp-assocC1C5C9.png", width = 780, height = 480, units = "px", pointsize = 12,
+    bg = "white",  res = NA)
 par(mfrow=c(1,1))
-manhattan(psassoc1, chr = "CHR", bp = "BP", p = "P", col = c("gray10", "gray60"), chrlabs = NULL,
-          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = NULL, logp = T,
-          annotatePval = NULL, annotateTop = T)
+manhattan(fall, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpsofinterest$SNP, logp = T,
+          annotatePval = 0.0000001, annotateTop = T, ylim = c(0, 9))
 dev.off()
 
-# Using all PCs
-psassoc2=read.table("post-impc1c5c9.assoc.logistic", header = T, as.is = T)
+############################# Using All PCs ######################################
+imassoc2=read.table("post-impc1-c10.assoc.logistic", header = T, as.is = T)
 
 # Now assess the genomic control inflation factor
-median(qchisq(psassoc2[,9], df=1, lower.tail = F), na.rm = T)/0.456
+median(qchisq(imassoc2[,9], df=1, lower.tail = F), na.rm = T)/0.456
 
 # Now produce association plot
-#snpsofinterest=assoc2[-log10(assoc2$P)>=7,]
-png("im2-assoc_qc.png", res = 1200, height = 5, width = 9, units = "in")
+snpsofinterest=imassoc2[-log10(imassoc2$P)>=7,]
+#png("im-assoc_qc.png", res = 1200, height =3, width = 6, units = "in")
+png(filename = "plink-imp-assocC1-C10.png", width = 780, height = 480, units = "px", pointsize = 12,
+    bg = "white",  res = NA)
 par(mfrow=c(1,1))
-manhattan(psassoc2, chr = "CHR", bp = "BP", p = "P", col = c("gray10", "gray60"), chrlabs = NULL,
-          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = NULL, logp = T,
-          annotatePval = NULL, annotateTop = T)
+manhattan(fall, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpsofinterest$SNP, logp = T,
+          annotatePval = 0.0000001, annotateTop = T, ylim = c(0, 9))
 dev.off()
 
-# Run Association for imputed dataset using additive MOI
-psassoc2=read.table("post-impc1-c10.assoc.logistic", header = T, as.is = T)
-png("im-add-assoc_qc.png", res = 1200, height = 5, width = 9, units = "in")
-par(mfrow=c(1,1))
-manhattan(psassoc2, chr = "CHR", bp = "BP", p = "P", col = c("gray10", "gray60"), chrlabs = NULL,
-          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = NULL, logp = T,
-          annotatePval = NULL, annotateTop = T)
-dev.off()
+######################## Using All PCs with different MOI #############################
+#imassoc3=read.table("post-impc1-c10-model-updated.model", header = T, as.is = T)
 
+# Now assess the genomic control inflation factor
+#median(qchisq(imassoc3[,9], df=1, lower.tail = F), na.rm = T)/0.456
 
-# Run Association for imputed dataset using hethom MOI
-psassoc2=read.table("post-impc1-c10-hethom-noNA.assoc.logistic", header = T, as.is = T)
-png("im-hethom-assoc_qc.png", res = 1200, height = 5, width = 9, units = "in")
+# Now produce association plot
+#snpsofinterest=imassoc3[-log10(imassoc3$P)>=7,]
+#png("im-assoc_qc.png", res = 1200, height =3, width = 6, units = "in")
+#png(filename = "plink-imp-assoc-model.png", width = 780, height = 480, units = "px", pointsize = 12,
+#    bg = "white",  res = NA)
+#par(mfrow=c(1,1))
+#manhattan(fall, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"),
+#          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpsofinterest$SNP, logp = T,
+#          annotatePval = 0.0000001, annotateTop = T, ylim = c(0, 9))
+#dev.off()
+
+############################ Using All PCs with hethom MOI ##########################
+imassoc4=read.table("post-impc1-c10-hethom-noNA.assoc.logistic", header = T, as.is = T)
+
+# Now assess the genomic control inflation factor
+#median(qchisq(imassoc4[,9], df=1, lower.tail = F), na.rm = T)/0.456
+
+# Now produce association plot
+snpsofinterest=imassoc4[-log10(imassoc4$P)>=7,]
+#png("im-assoc_qc.png", res = 1200, height =3, width = 6, units = "in")
+png(filename = "plink-imp-assoc-hethom.png", width = 780, height = 480, units = "px", pointsize = 12,
+    bg = "white",  res = NA)
 par(mfrow=c(1,1))
-manhattan(psassoc2, chr = "CHR", bp = "BP", p = "P", col = c("gray10", "gray60"), chrlabs = NULL,
-          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = NULL, logp = T,
-          annotatePval = NULL, annotateTop = T)
+manhattan(fall, chr = "CHR", bp = "BP", p = "P", col = c("blue4", "orange3"),
+          suggestiveline = -log10(1e-05),  genomewideline = -log10(5e-08), highlight = snpsofinterest$SNP, logp = T,
+          annotatePval = 0.0000001, annotateTop = T, ylim = c(0, 9))
 dev.off()
 
 
 # Plot a Q-Q plot for the association analysis
 png("qq_plots.png", res=1200, height=7, width=7, units="in")
 par(mfrow=c(2,2))
-qq(psassoc$P, main="Q-Q plot after QC")
-qq(psassoc1$P, main="Q-Q plot after QC")
-qq(psassoc2$P, main="Q-Q plot after QC")
+qq(imassoc$P, main="Q-Q plot after QC")
+qq(imassoc1$P, main="Q-Q plot after QC")
+qq(imassoc2$P, main="Q-Q plot after QC")
+qq(imassoc4$P, main="Q-Q plot after QC")
 dev.off()
 
 
