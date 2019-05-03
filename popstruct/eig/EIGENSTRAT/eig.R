fb5ccbefbfd0f23287d9a35f30f781eaf7f994f2
esohkevin <esohkevin4@gmail.com>
2019-05-03 08:18:07 +0300

    update

--- popstruct/eig/EIGENSTRAT/eig.R
+++ popstruct/eig/EIGENSTRAT/eig.R
@@ -6,7 +6,7 @@ evecDat = read.table(fn, col.names=c("Sample", "PC1", "PC2", "PC3",
 				     "PC4", "PC5", "PC6", "PC7", "PC8", 
 				     "PC9", "PC10", "Status"))
 
-# Import Population groups from popstruct directory
+############## Import Population groups from popstruct directory ################
 popGroups = read.table("../../qc-camgwas.pops", col.names=c("Sample", "PopGroup"))
 mergedEvecDat = merge(evecDat, popGroups, by="Sample")
 
@@ -18,15 +18,15 @@ par(mfrow=c(2,1))
 #  cex=0.3, mai=c(0.2,0.3,0.3,0.3)
 plot(evecDat$PC1, evecDat$PC2, xlab="eigenvalue1", ylab="eigenvalue2", main="Eigenanalysis With Sample Status")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
-d = evecDat[evecDat$Status=="Control",]
 points(d$PC1, d$PC2, col=2, pch=20)
-legend("topleft", c("Case", "Control"), col=c(1,2), pch=20)
+d = evecDat[evecDat$Status=="Control",]
+points(d$PC1, d$PC2, col=1, pch=20)
+legend("topleft", c("Case", "Control"), col=c(2,1), pch=20)
 plot(mergedEvecDat$PC1, mergedEvecDat$PC2, col=mergedEvecDat$PopGroup, main="Eigenanalysis With Sample Region", xlab="eigenvalue1", ylab="eigenvalue2", pch=20)
 legend("topleft", legend=levels(mergedEvecDat$PopGroup), col=1:length(levels(mergedEvecDat$PopGroup)), pch=20)
 dev.off()
 
-## Plot for 6 pairs of eigenvalues
+################### Plot for 6 pairs of eigenvalues #######################
 png(filename = "eigenv1-v10.png", width = 890, height = 600, units = "px", pointsize = 12,
     bg = "white",  res = NA, type = c("cairo-png"))
 par(mfrow=c(2,3))
@@ -34,46 +34,46 @@ par(mfrow=c(2,3))
 #par(mfrow=c(2,3), cex=0.3, cex.lab=0.8, cex.axis=0.8, cex.main=0.8)
 plot(evecDat$PC1, evecDat$PC2, xlab="PC1", ylab="PC2", pch=20, main="PC1 Vs PC2")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
-d = evecDat[evecDat$Status=="Control",]
 points(d$PC1, d$PC2, col=2, pch=20)
-legend("topleft", c("Case", "Control"), col=c(1,2), pch=20, bty="n")
+d = evecDat[evecDat$Status=="Control",]
+points(d$PC1, d$PC2, col=1, pch=20)
+legend("topleft", c("Case", "Control"), col=c(2,1), pch=20, bty="n")
 plot(evecDat$PC3, evecDat$PC4, xlab="PC3", ylab="PC4", pch=20, main="PC3 Vs PC4")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
+points(d$PC3, d$PC4, col=2, pch=20)
 d = evecDat[evecDat$Status=="Control",]
-points(d$PC1, d$PC2, col=2, pch=20)
-legend("topright", c("Case", "Control"), col=c(1,2), pch=20, bty="n")
+points(d$PC3, d$PC4, col=1, pch=20)
+legend("topright", c("Case", "Control"), col=c(2,1), pch=20, bty="n")
 plot(evecDat$PC5, evecDat$PC6, xlab="PC5", ylab="PC6", pch=20, main="PC5 Vs PC6")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
+points(d$PC5, d$PC6, col=2, pch=20)
 d = evecDat[evecDat$Status=="Control",]
-points(d$PC1, d$PC2, col=2, pch=20)
-legend("topright", c("Case", "Control"), col=c(1,2), pch=20, bty="n")
+points(d$PC5, d$PC6, col=1, pch=20)
+legend("topright", c("Case", "Control"), col=c(2,1), pch=20, bty="n")
 plot(evecDat$PC7, evecDat$PC8, xlab="PC7", ylab="PC8", pch=20, main="PC7 Vs PC8")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
+points(d$PC7, d$PC8, col=2, pch=20)
 d = evecDat[evecDat$Status=="Control",]
-points(d$PC1, d$PC2, col=2, pch=20)
-legend("topright", c("Case", "Control"), col=c(1,2), pch=20, bty="n")
+points(d$PC7, d$PC8, col=1, pch=20)
+legend("topright", c("Case", "Control"), col=c(2,1), pch=20, bty="n")
 plot(evecDat$PC9, evecDat$PC10, xlab="PC9", ylab="PC10", pch=20, main="PC9 Vs PC10")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
+points(d$PC9, d$PC10, col=2, pch=20)
 d = evecDat[evecDat$Status=="Control",]
-points(d$PC1, d$PC2, col=2, pch=20)
-legend("topright", c("Case", "Control"), col=c(1,2), pch=20, bty="n")
+points(d$PC9, d$PC10, col=1, pch=20)
+legend("topright", c("Case", "Control"), col=c(2,1), pch=20, bty="n")
 plot(evecDat$PC1, evecDat$PC5, xlab="PC1", ylab="PC5", pch=20, main="PC1 Vs PC5")
 d = evecDat[evecDat$Status=="Case",]
-points(d$PC1, d$PC2, col=1, pch=20)
+points(d$PC1, d$PC5, col=2, pch=20)
 d = evecDat[evecDat$Status=="Control",]
-points(d$PC1, d$PC2, col=2, pch=20)
-legend("topleft", c("Case", "Control"), col=c(1,2), pch=20, bty="n")
+points(d$PC1, d$PC5, col=1, pch=20)
+legend("topleft", c("Case", "Control"), col=c(2,1), pch=20, bty="n")
 dev.off()
 
-# Import the data file containing ethnicity column
+############# Import the data file containing ethnicity column #################
 evecthn=read.table("qc-camgwas-ethni.evec", header=T, as.is=T)
 
-## Plot First 2 evecs with ethnicity distinction
+######### Plot First 2 evecs with ethnicity distinction ##########
 png(filename = "evec_with_ethn.png", width = 510, height = 510, units = "px", pointsize = 12,
     bg = "white",  res = NA, type = c("quartz"))
 plot(evecthn$PC1, evecthn$PC2, col=as.factor(evecthn$ethnicity), main="Eigenanalysis With Ethnicity", 
@@ -81,3 +81,63 @@ plot(evecthn$PC1, evecthn$PC2, col=as.factor(evecthn$ethnicity), main="Eigenanal
 legend("topleft", legend=levels(as.factor(evecthn$ethnicity)), 
        col=1:length(levels(as.factor(evecthn$ethnicity))), pch=20)
 dev.off()
+
+##### Project Case-Control status and Ethnicity Along the three interesting eigenvalues ######
+png(filename = "eigenv-select.png", width = 890, height = 600, units = "px", pointsize = 12,
+    bg = "white",  res = NA, type = c("cairo-png"))
+par(mfrow=c(2,3))
+
+# Pour Ethnic affiliations
+plot(evecthn$PC1, evecthn$PC2, xlab="PC1", ylab="PC2", pch=20, main="PC1 Vs PC2 - Ethnicity")
+d = evecthn[evecthn$ethnicity=="BA",]
+points(d$PC1, d$PC2, col=2, pch=20)
+d = evecthn[evecthn$ethnicity=="SB",]
+points(d$PC1, d$PC2, col=1, pch=20)
+d = evecthn[evecthn$ethnicity=="FO",]
+points(d$PC1, d$PC2, col=3, pch=20)
+legend("topleft", c("BA", "SB", "FO"), col=c(2,1,3), pch=20, bty="n")
+plot(evecthn$PC3, evecthn$PC4, xlab="PC3", ylab="PC4", pch=20, main="PC3 Vs PC4 - Ethnicity")
+d = evecthn[evecthn$ethnicity=="BA",]
+points(d$PC3, d$PC4, col=2, pch=20)
+d = evecthn[evecthn$ethnicity=="SB",]
+points(d$PC3, d$PC4, col=1, pch=20)
+d = evecthn[evecthn$ethnicity=="FO",]
+points(d$PC3, d$PC4, col=3, pch=20)
+legend("topright", c("BA", "SB", "FO"), col=c(2,1,3), pch=20, bty="n")
+plot(evecthn$PC1, evecthn$PC5, xlab="PC1", ylab="PC5", pch=20, main="PC1 Vs PC5 - Ethnicity")
+d = evecthn[evecthn$ethnicity=="BA",]
+points(d$PC1, d$PC2, col=2, pch=20)
+d = evecthn[evecthn$ethnicity=="SB",]
+points(d$PC1, d$PC5, col=1, pch=20)
+d = evecthn[evecthn$ethnicity=="FO",]
+points(d$PC1, d$PC5, col=3, pch=20)
+legend("topleft", c("BA", "SB", "FO"), col=c(2,1,3), pch=20, bty="n")
+
+# Involving mixed reports
+plot(mergedEvecDat$PC1, mergedEvecDat$PC2, xlab="PC1", ylab="PC2", pch=20, main="PC1 Vs PC2 - Population Group")
+d = mergedEvecDat[mergedEvecDat$PopGroup=="BUE",]
+points(d$PC1, d$PC2, col=4, pch=20)
+d = mergedEvecDat[mergedEvecDat$PopGroup=="DOU",]
+points(d$PC1, d$PC2, col=5, pch=20)
+d = mergedEvecDat[mergedEvecDat$PopGroup=="YDE",]
+points(d$PC1, d$PC2, col=6, pch=20)
+legend("topleft", c("BUE", "DOU", "YDE"), col=c(4,5,6), pch=20, bty="n")
+plot(mergedEvecDat$PC3, mergedEvecDat$PC4, xlab="PC3", ylab="PC4", pch=20, main="PC3 Vs PC4 - Population Group")
+d = mergedEvecDat[mergedEvecDat$PopGroup=="BUE",]
+points(d$PC3, d$PC4, col=4, pch=20)
+d = mergedEvecDat[mergedEvecDat$PopGroup=="DOU",]
+points(d$PC3, d$PC4, col=5, pch=20)
+d = mergedEvecDat[mergedEvecDat$PopGroup=="YDE",]
+points(d$PC3, d$PC4, col=6, pch=20)
+legend("topright", c("BUE", "DOU", "YDE"), col=c(4,5,6), pch=20, bty="n")
+plot(mergedEvecDat$PC1, mergedEvecDat$PC5, xlab="PC1", ylab="PC5", pch=20, main="PC1 Vs PC5 - Population Group")
+d = mergedEvecDat[mergedEvecDat$PopGroup=="BUE",]
+points(d$PC1, d$PC5, col=4, pch=20)
+d = mergedEvecDat[mergedEvecDat$PopGroup=="DOU",]
+points(d$PC1, d$PC5, col=5, pch=20)
+d = mergedEvecDat[mergedEvecDat$PopGroup=="YDE",]
+points(d$PC1, d$PC5, col=6, pch=20)
+legend("topleft", c("BUE", "DOU", "YDE"), col=c(4,5,6), pch=20, bty="n")
+
+dev.off()
+
