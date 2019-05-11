774a27f39c1df7a8d59ab41437d0e4ef1206776e
esohkevin <esohkevin4@gmail.com>
2019-05-11 23:47:15 +0000

    update

--- assoc_results/run_snptest_assoc.R
+++ assoc_results/run_snptest_assoc.R
@@ -1,9 +1,9 @@
 #!/usr/bin/Rscript
 
-assoc.imp.snptest <- read.table("snptest_assoc_test_imp.txt", header=T, as.is=T)
+assoc.imp.snptest <- read.table("../michigan/eagle/snptest-sm-assoc.txt", header=T, as.is=T)
 png("imp_snptest_assoc.png")
 plot(assoc.imp.snptest$position, 
-	assoc.imp.snptest$bayesian_add_log10_bf, 
+	assoc.imp.snptest$bayesian_dom_log10_bf, 
 	ylim=c(0,10), main = "Typed + Imputed SNPs", 
 	xlab="Position", ylab="log10 Bayes factor", 
 	col = 1+1*(assoc.imp.snptest[,1] == "---"), pch=20)
--- phase/run_imput2.sh
+++ phase/run_imput2.sh
@@ -3,18 +3,47 @@
 ref_path="1000GP_Phase3/"
 
 for chr in {1..22}; do
+
   if [[ -f "chr${chr}-phased_wref.haps" ]]; then
-   impute_v2.3.2 \
-      -use_prephased_g \
-      -known_haps_g chr"${chr}"-phased_wref.haps \
-      -m "$ref_path"genetic_map_chr"${chr}"_combined_b37.txt \
-      -h "$ref_path"1000GP_Phase3_chr"${chr}".hap.gz \
-      -l "$ref_path"1000GP_Phase3_chr"${chr}".legend.gz \
-      -int 202658471 204658471 \
-      -Ne 17469 \
-      -buffer 1000 \
-      -o chr"${chr}"_test_imput2.gen
-  else
-    echo "chr"${chr}"-phased_wref.haps could not be found!"
-  fi
+
+     rm chr${chr}_chunks.txt
+
+     for interval in `cat chr${chr}intervals.txt`; do
+	echo -e """-use_prephased_g -known_haps_g chr"${chr}"-phased_wref.haps -m "$ref_path"genetic_map_chr"${chr}"_combined_b37.txt -h "$ref_path"1000GP_Phase3_chr"${chr}".hap.gz -l "$ref_path"1000GP_Phase3_chr"${chr}".legend.gz  -int "${interval}" -Ne 20000 -buffer 500 -o chr"${chr}"_"${interval}"_imputed.gen\n""" >> chr${chr}_imput_chunks.txt
+     done
+
+     sed 's/=/ /g' chr${chr}_imput_chunks.txt > chr${chr}_chunks.txt; # Primary aim is to create this file for all chrs to use for parallele jod execution
+     rm chr${chr}_imput_chunks.txt
+
+     # Run IMPUTE2 with the commands in the files previously created
+     cat chr${chr}_chunks.txt | xargs -P6 -n12 impute_v2.3.2 &
+
+     else
+
+     echo "chr"${chr}"-phased_wref.haps could not be found!"
+
+   fi
 done
+
+
+
+
+
+
+
+#for chr in {1..22}; do
+#  if [[ -f "chr${chr}-phased_wref.haps" ]]; then
+#   impute_v2.3.2 \
+#      -use_prephased_g \
+#      -known_haps_g chr"${chr}"-phased_wref.haps \
+#      -m "$ref_path"genetic_map_chr"${chr}"_combined_b37.txt \
+#      -h "$ref_path"1000GP_Phase3_chr"${chr}".hap.gz \
+#      -l "$ref_path"1000GP_Phase3_chr"${chr}".legend.gz \
+#      -int '160000000 162000000' \
+#      -Ne 17469 \
+#      -buffer 1000 \
+#      -o chr"${chr}"_imputed2.gen
+#  else
+#    echo "chr"${chr}"-phased_wref.haps could not be found!"
+#  fi
+#done
