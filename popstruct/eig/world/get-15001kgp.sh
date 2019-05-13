a03e1b82a75f97e78002f991b121dbfc99d61541
esohkevin <esohkevin4@gmail.com>
2019-05-14 00:55:55 +0300

    update

--- popstruct/eig/world/get-15001kgp.sh
+++ popstruct/eig/world/get-15001kgp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# --thin-indiv-count 1500 \ add this line to get only 1500 individuals from 1KGP
+
+# Get 1kgp individuals limiting to only common SNPs that are found in qc-camgwas data
 plink \
 	--vcf ../../../1000G/Phase3_merged.vcf.gz \
-	--thin-indiv-count 1500 \
 	--autosome \
 	--extract updated-qc.rsids \
 	--keep-allele-order \
@@ -15,16 +17,27 @@ plink \
 	--biallelic-only \
 	--out qc-rsids-world-pops
 
-# Merge qc-data with common SNPs
+# Get a subset of individuals from qc-camgwas to merge with individuals from 1kgp
 plink \
 	--bfile ../../../analysis/qc-camgwas-updated \
+	--allow-no-sex \
+	--thin-indiv-count 300 \
+	--make-bed \
+	--out thinned-qc-camgwas
+
+# Get the rsIDs of the SNPs in the thinned-qc-camgwas file to use for merging with world pops
+cut -f2 thinned-qc-camgwas.bim > thinned-qc-rs.ids
+
+# Merge thinned qc-data with common SNPs
+plink \
+	--bfile thinned-qc-camgwas \
 	--allow-no-sex \
 	--bmerge qc-rsids-world-pops \
 	--make-bed \
 	--out qc-world-merge \
-	--extract ../../qc-rs.ids
+	--extract thinned-qc-rs.ids
 
-# Prune to get only SNPs with near linkage equilibrium
+# Prune to get only SNPs at linkage equilibrium (independent SNPs - no LD between them)
 plink \
 	--bfile qc-world-merge \
 	--indep-pairwise 50 5 0.2 \
