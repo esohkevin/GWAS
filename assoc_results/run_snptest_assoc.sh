38d289d4f42163e49f981c675f7f52589bd4aef2
esohkevin <esohkevin4@gmail.com>
2019-05-13 14:31:24 +0000

    update

--- assoc_results/run_snptest_assoc.sh
+++ assoc_results/run_snptest_assoc.sh
@@ -6,16 +6,19 @@ phase_path="$HOME/GWAS/Git/GWAS/phase/imputed/"
 # Perform only bayesian for now. Add '-frequentist 1 2 3 5 \' to the command line to perform frequentist tests
 
 for chunk in "${phase_path}"chr1_*_imputed.gen; do
-    snptest_v2.5.4-beta3 \
-	-data "${chunk}" "${sample_path}"qc-camgwas.sample \
-	-bayesian 1 2 3 5 \
-	-method score \
-	-pheno pheno1 \
-	-cov_all \
-	-o ${chunk/_imputed.gen/_assoc.txt}
+#    snptest_v2.5.4-beta3 \
+#	-data "${chunk}" "${sample_path}"qc-camgwas.sample \
+#	-bayesian 1 2 3 5 \
+#	-method score \
+#	-pheno pheno1 \
+#	-cov_all \
+#	-o ${chunk/_imputed.gen/_assoc.txt}
+
+#mv ${phase_path}*_assoc.txt .											# Move association results to current working directory
+
+Rscript --vanilla run_snptest_assoc.R ${chunk/_imputed.gen/_assoc.txt} ${chunk/_imputed.gen/_assoc.png}		# Produce R plots of the chunks
+
 done
 
-mv ${phase_path}*_assoc.txt .
-#cat chr1_114000000_assoc.txt
-
-#Rscript --vanilla run_snptest_assoc.R ../phase/chr2_84000001_imputed.gen chr2_84000001_imputed.png	# Uncomment this line to produce R plots of the chunks
+#cat chr1_114000000_assoc.txt											# Merge association chuncks for each Chromosome
+mv ../phase/imputed/*.png .
