1bc16a7e1c5bb673344e84e153bc4f391651aaf9
esohkevin <esohkevin4@gmail.com>
2019-05-17 01:16:14 +0000

    update

---
+++ run_camgwas.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+###############################################################################################
+##											     ##
+##			Script to Run the entire CamGwas Pipeline			     ##
+##											     ##
+###############################################################################################
+
+############################################ PATHS ############################################
+#
+base="`pwd`"
+analysis="${base}analysis/"
+assocResults="${base}assoc_results/"
+popstruct="${base}popstruct/eig/"
+phase="${base}phase/"
+1kgp="${base}1000G/"
+images="${base}images/"
+michigan="${base}michigan/"
+samger="${base}sanger/"
+samples="${base}samples/"
+#
+############################################### QC ############################################
+#
+cd ${analysis}
+
+./qc-pipline.sh
+
+cd ${popstruct}
+#
+####################################### Population Structure ##################################
+#
+./run_popstruct.sh
+
+cd ${analysis}
+#
+################################ Prepare Dataset for Imputation ###############################
+#
+./imputPrep.sh
+
+cd ${phase}
+#
+##################################### Phasing and Imputation ##################################
+#
+./run_phasImput.sh
+
