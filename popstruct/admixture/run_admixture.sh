cb1134f4b41e37f404ff222070b8347cc8e9733a
esohkevin <esohkevin4@gmail.com>
2019-05-24 04:12:24 +0300

    update run_admixture.sh

--- popstruct/admixture/run_admixture.sh
+++ popstruct/admixture/run_admixture.sh
@@ -2,22 +2,22 @@
 
 ## Thin qc-camgwas-updated dataset for LD
 # Prune the qc-dataset for SNPs within 50bp with r^2 < 0.2 using a window of 5 SNPs
-#plink \
-#        --bfile ../../analysis/qc-camgwas-updated \
-#        --indep-pairwise 50 5 0.2 \
-#        --allow-no-sex \
-#        --autosome \
-#        --biallelic-only \
-#        --out qc-ldPruned
+plink \
+        --bfile admix-data \
+        --indep-pairwise 50 5 0.2 \
+        --allow-no-sex \
+        --autosome \
+        --biallelic-only \
+        --out qc-ldPruned
 
-#plink \
-#        --bfile ../../analysis/qc-camgwas-updated \
-#        --extract qc-ldPruned.prune.in \
-#        --allow-no-sex \
-#        --autosome \
-#	--keep-allele-order \
-#        --make-bed \
-#        --out qc-camgwas-ldPruned
+plink \
+        --bfile ../../analysis/qc-camgwas-updated \
+        --extract qc-ldPruned.prune.in \
+        --allow-no-sex \
+        --autosome \
+	--keep-allele-order \
+        --make-bed \
+        --out qc-camgwas-ldPruned
 #
 #plink \
 #	--bfile qc-camgwas-ldPruned \
@@ -28,11 +28,13 @@
 #	--out qc-camgwas-ldPruned
 
 # Perform admixture cross-validation to determine the appropriate k value to use
-for k in $(seq 1 5); do
-	admixture --cv qc-camgwas-ldPruned.bed "${k}" | tee log${k}.out;
-done
+#for k in $(seq 1 5); do
+#	admixture --cv qc-camgwas-ldPruned.bed "${k}" | tee log${k}.out;
+#done
 
 echo -e "\nDone Cross-Validating!"
 echo "Please inspect the results and make a choice of k and"
 echo "run admixture with your choice"
 
+
+admixture --cv qc-camgwas-ldPruned.bed 2 | tee log.out		# No crossvalidation
