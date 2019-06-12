13d9c29be3feb9d570e4d8af7f1fe8ec8916aea1
esohkevin <esohkevin4@gmail.com>
2019-06-12 09:06:28 +0000

    add files

--- popstruct/ld_analysis/plot_heatmap.R
+++ popstruct/ld_analysis/plot_heatmap.R
@@ -1,11 +1,17 @@
 #!/usr/bin/Rscript
 
+args <- commandArgs(TRUE)
+
+fn <- args[1]
+
+ldMatrix <- as.matrix(read.table(fn, fill=T, skip = 0))
+
 # PLot heatmap with upper triangular matrix of ld analysis
-png(filename = "chr_mhc_hg19_heatmap.png", width = 800, height = 600, units = "px", pointsize = 12,
+png(args[2], width = 800, height = 600, units = "px", pointsize = 12,
     bg = "white",  res = NA)
 #ldMatrix <- as.matrix(read.table("chr6Mhc.ld", fill=T, skip = 0))
 
-ldMatrix <- readBin('chr6Mhc-bin.ld.bin', what="numeric",n=9591, size=4)
+#ldMatrix <- readBin('chr6Mhc-bin.ld.bin', what="numeric",n=9591, size=4)
 
 #heatmap(ldMatrix)
 image(lower.tri(ldMatrix))
