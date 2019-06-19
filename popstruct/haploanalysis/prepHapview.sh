7873318d028c856728107c3ad278c271be611ea5
esohkevin <esohkevin4@gmail.com>
2019-06-19 20:38:16 +0000

    updates

---
+++ popstruct/haploanalysis/prepHapview.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+for pop in cam yri esn gwd msl lwk; do
+    plink \
+	--bfile ${pop} \
+	--recode HV-1chr \
+	--chr 11 \
+	--from-kb 5200 \
+	--to-kb 5400 \
+	--keep-allele-order \
+	--out ~/bioTools/haploview/${pop}
+done
