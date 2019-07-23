c7b70ba708f9ac1923d55c72d38b96e2ea814ab5
esohkevin <esohkevin4@gmail.com>
2019-07-23 23:59:26 +0000

    add simhap.sh

---
+++ bin/simhap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hapgen2 \
+	-m /home/esoh/esohdata/GWAS/phase/1000GP_Phase3/genetic_map_chr11_combined_b37.txt \
+	-l chr11.legend \
+	-h chr11.haps \
+	-n 1000 1000 \
+	-dl 56216495 0 1 1 \
+	-no_gens_output \
+	-Ne 17469 \
+	-o chr11.out
+
+sed '1d' chr11.out.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > chr11con.map
+sed 's/0/2/g' chr11.out.controls.haps > chr11con.hap
