deb7c50efe33951011c03fd066d37ea6680b0e81
esohkevin <esohkevin4@gmail.com>
2019-07-23 23:56:54 +0000

    updates

--- popstruct/plink2rehh.sh
+++ popstruct/plink2rehh.sh
@@ -1,37 +1,77 @@
 #!/bin/bash
 
-if [[ $# != 4 ]]; then
+param="$1"
+
+if [[ "$param" != [123] ]]; then
+   
    echo """
-	Usage: ./rehhPrep.sh <chr#> <from-kb> <to-kb> <pop-name>
+	Usage: ./plink2rehh.sh [1|2|3] : Enter '1' if you are working with a specific chromosome region (e.g. chr11:5200-5600)
+					       '2' if you are working with a single whole chromosome
+					       '3' if you are working with a whole genome with more than one chromosomes
    """
 else
+   if [[ "$param" == "1" && $# != 7 ]]; then
+      echo """
+      	Usage: ./plink2rehh.sh 1 <chr#> <from-kb> <to-kb> <pop-name> <genomic-region-name> <input-VCF> (for specific chromosome region)
+      """
+   elif [[ "$param" == "1" && $# == 7 ]]; then
 
-   # Chr11 HBB 2Mb (5200-5400) region
-   plink2 \
-   	--chr $1 \
-   	--export hapslegend \
-   	--vcf chr${1}-phased_wref.vcf.gz \
-   	--out chr${1}${4}hbb \
-   	--from-kb $2 \
-   	--to-kb $3 \
-   	--keep $4.txt \
-   	--double-id
+      # Chr11 HBB 2Mb (5200-5400) region
+      plink2 \
+      	--chr $2 \
+      	--export hapslegend \
+      	--vcf $7 \
+      	--out chr${2}${5}${6} \
+      	--from-kb $3 \
+      	--to-kb $4 \
+      	--keep $5.txt \
+      	--double-id
+      
+      sed '1d' chr${2}${5}${6}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > chr${2}${5}${6}.map
+      sed 's/0/2/g' chr${2}${5}${6}.haps > chr${2}${5}${6}.hap
    
-   sed '1d' chr${1}${4}hbb.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > chr${1}${4}hbb.map
-   sed 's/0/2/g' chr${1}${4}hbb.haps > chr${1}${4}hbb.hap
-   
-   # Entire chr11
-   plink2 \
-           --chr $1 \
-           --export hapslegend \
-           --vcf chr${1}-phased_wref.vcf.gz \
-           --out chr$1$4 \
-           --keep $4.txt \
-           --double-id
+   elif [[ "$param" == "2" && $# != 5 ]]; then
+      echo """
+           Usage: ./plink2rehh.sh 2 <chr#> <pop-name> <input-VCF> (for single whole chromosome)
+      """
   
-   sed '1d' chr${1}${4}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > chr${1}${4}.map
-   sed 's/0/2/g' chr${1}${4}.haps > chr${1}${4}.hap
+   elif [[ "$param" == "1" && $# == 5 ]]; then
+
+      # Entire chr11
+      plink2 \
+              --chr $2 \
+              --export hapslegend \
+              --vcf $4 \
+              --out chr$2$3 \
+              --keep $3.txt \
+              --double-id
+     
+      sed '1d' chr${2}${3}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > chr${2}${3}.map
+      sed 's/0/2/g' chr${2}${3}.haps > chr${2}${3}.hap
+      
    
-   rm *.haps *.sample *.legend *.log
+   elif [[ "$param" == "3" && $# != 4 ]]; then
+      echo """
+           Usage: ./plink2rehh.sh 3 <pop-name> <input-VCF> (whole genome with more than one chromosomes)
+      """
+    elif [[ "$param" == "1" && $# == 4 ]]; then
+      # Entire dataset with more than one chromosomes
+      plink2 \
+              --export hapslegend \
+              --vcf $3 \
+              --out $2 \
+              --keep $2.txt \
+              --double-id
+     
+      sed '1d' ${3}.legend | awk '{print $1"\t""11""\t"$2"\t"$4"\t"$3}' > ${3}.map
+      sed 's/0/2/g' ${3}.haps > ${3}.hap
+   
+   fi
+
+   for file in *.haps *.legend *.log *.sample; do
+       if [[ -f ${file} ]]; then
+         rm ${file}
+       fi
+   done
 
 fi
