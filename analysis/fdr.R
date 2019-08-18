daae84a217648335342f914929279e23d01a2df7
esohkevin <esohkevin4@gmail.com>
2019-08-18 07:39:33 +0000

    add fdr.R

--- analysis/fdr.R
+++ analysis/fdr.R
@@ -9,12 +9,12 @@ calc_fdr <- function(assoc.dat = assoc_result) {
 	out_name <- paste(assoc.dat,".adj",sep="")
 
 	assoc <- read.table(assoc.dat, header=T, as.is=T)
-	assoc$P_adj_BH <- p.adjust(as.vector(P), method="BH")		# Benjamini & Hochberg
-	assoc$P_adj_Bonf <- p.adjust(as.vector(P), method="BF")		# Bonferroni
-	assoc$P_adj_BY <- p.adjust(as.vector(P), method="By")   	# Benjamini & Yekutieli
-	assoc$P_adj_Holm <- p.adjust(as.vector(P), method="holm")   	# Holm
-        assoc$P_adj_Hommel <- p.adjust(as.vector(P), method="hommel")  	# Hommel
-        assoc$P_adj_Hochberg <- p.adjust(as.vector(P), method="hochberg")   	# Hochberg
+	assoc$P_adj_BH <- p.adjust(as.vector(assoc$P), method="BH")		# Benjamini & Hochberg
+	assoc$P_adj_Bonf <- p.adjust(as.vector(assoc$P), method="BF")		# Bonferroni
+	assoc$P_adj_BY <- p.adjust(as.vector(assoc$P), method="By")   		# Benjamini & Yekutieli
+	assoc$P_adj_Holm <- p.adjust(as.vector(assoc$P), method="holm")   	# Holm
+        assoc$P_adj_Hommel <- p.adjust(as.vector(assoc$P), method="hommel")  	# Hommel
+        assoc$P_adj_Hochberg <- p.adjust(as.vector(assoc$P), method="hochberg") # Hochberg
 	write.table(assoc, file=out_name, col.names=TRUE, row.names-FALSE)
 }
 
