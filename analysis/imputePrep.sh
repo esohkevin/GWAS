b1feb46d084666d78eaad0264b079301b185ca01
esohkevin <esohkevin4@gmail.com>
2019-08-30 12:54:42 +0000

    updates

--- analysis/imputePrep.sh
+++ analysis/imputePrep.sh
@@ -3,10 +3,12 @@
 kgp="../phase/1000GP_Phase3/"
 eig="${baseDir}/popstruct/eig/"
 samples="../samples/"
+k="../1000G/"
 
-echo """ 
+echo -e """\e[1m\e[38;5;40m
 	 Extracting eigen-corrected samples
 	------------------------------------
+	\e[0m
 """
 plink \
         --bfile qc-camgwas \
@@ -15,9 +17,10 @@ plink \
 	--autosome \
         --out qc-camgwas-eig-corr
 
-echo """ 
+echo -e """\e[1m\e[38;5;40m 
 	 Getting Palimcromic SNPs
 	--------------------------
+	\e[0m
 """
 # Compute allel frequencies
 plink \
@@ -26,15 +29,17 @@ plink \
 	--freq \
 	--out qc-camgwas-eig-corr
 
-echo -e """
+echo -e """\e[1m\e[38;5;40m
 	 Extracting palindromic SNPs in R for subsequent exclusion
 	------------------------------------------------------------
+	\e[0m
 """
 R CMD BATCH palindromicsnps.R
 
-echo -e """
+echo -e """\e[1m\e[38;5;40m
 	 Checking Strand using the Wrayner perl script
 	------------------------------------------------
+	\e[0m
 """
 ./checkstrand.sh
 
@@ -44,12 +49,15 @@ plink --bfile TEMP0 --allow-no-sex --exclude Exclude-qc-camgwas-eig-corr-1000G.t
 plink --bfile TEMP1 --update-chr Chromosome-qc-camgwas-eig-corr-1000G.txt 2 1 --make-bed --out TEMP2
 plink --bfile TEMP2 --update-map Position-qc-camgwas-eig-corr-1000G.txt 2 1 --make-bed --out TEMP3
 plink --bfile TEMP3 --flip Strand-Flip-qc-camgwas-eig-corr-1000G.txt --make-bed --out TEMP4
-plink2 --bfile TEMP4 --ref-allele force refSites.txt 4 1 --make-bed --out qc-camgwas-updated
-#plink --bfile TEMP5 --update-name ID-qc-camgwas-eig-corr-1000G.txt 2 1 --keep-allele-order --make-bed --out qc-camgwas-updated
+#plink2 --bfile TEMP4 --ref-allele force refSites.txt 4 1 --make-bed --out qc-camgwas-updated
+plink2 --bfile TEMP4 --ref-allele force chimp_anc.txt 3 2 --make-bed --out TEMP5
+plink2 --bfile TEMP5 --ref-allele force ancSites.txt 4 3 --make-bed --out qc-camgwas-updated
 
-echo -e """
+
+echo -e """\e[1m\e[38;5;40m
 	 Converting plink binary files to VCF
 	--------------------------------------
+	\e[0m
 """
 #./plink2vcf.sh
 
@@ -57,7 +65,6 @@ plink \
 	--bfile qc-camgwas-updated \
 	--recode vcf-fid bgz \
 	--real-ref-alleles \
-	--keep-allele-order \
 	--out qc-camgwas-updated
 
 tabix -f -p vcf qc-camgwas-updated.vcf.gz
