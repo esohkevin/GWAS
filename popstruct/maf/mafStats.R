fc5afb33a6744cb91b9877da84a2745a85726039
esohkevin <esohkevin4@gmail.com>
2019-09-04 17:32:24 +0300

    add images, update mafStats.R

--- popstruct/maf/mafStats.R
+++ popstruct/maf/mafStats.R
@@ -6,7 +6,7 @@ maf <- fread("freqs_camgwas.txt")
 colnames(maf) <- c("CHR","POS","RAF","MAF")
 attach(maf)
 
-png("maf.png", height=15, width=15, units="cm", res=300, points=14)
+png("maf.png", height=10, width=10, units="cm", res=100, points=12)
 hist(MAF, ylim = c(0,150000), breaks = 100, main = "MAF Spectrum")
 abline(v=c(0.01,0.05,0.50), col=c(2,4,1), lty=2)
 dev.off()
