259d9e5ed29478aa61cb796c2eeb8f87d196c1d7
esohkevin <esohkevin4@gmail.com>
2019-09-23 02:33:26 +0000

    updates

---
+++ phase/fs_4.0.1/project/plink2cp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+analysis="../../../analysis/"
+pop="../../../popstruct/"
+
+../popstruct/fst/fvcf/
+
+for chr in {1..22}; do
+    plink \
+        --vcf ../fs.vcf.gz \
+        --allow-no-sex \
+        --pheno "${analysis}"raw-camgwas.fam \
+        --update-sex "${analysis}"raw-camgwas.fam 3 \
+        --mpheno 4 \
+	--maf 0.05 \
+        --recode12 \
+        --chr ${chr} \
+        --double-id \
+        --biallelic-only \
+        --keep-allele-order \
+        --out chr${chr}_phasedWref
+
+    plink2chromopainter.pl \
+	-p=chr${chr}_phasedWref.ped \
+	-m=chr${chr}_phasedWref.map \
+	-o=chr${chr}_phasedWref.phase \
+	-d=chr${chr}_phasedWref.cp.sample \
+	-g=10e6
+
+    makeuniformrecfile.pl chr${chr}_phasedWref.phase chr${chr}_phasedWref.recombfile
+    rm chr${chr}_phasedWref.map chr${chr}_phasedWref.ped chr${chr}_phasedWref.nosex chr${chr}_phasedWref.log
+done
+
+mv chr1_phasedWref.cp.sample camgwasPhasedWref.ids
+rm chr*_phasedWref.cp.sample
