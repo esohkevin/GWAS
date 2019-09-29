190ff99ab58c7b20c2872e373ed90804ad552bff
esohkevin <esohkevin4@gmail.com>
2019-09-29 04:20:49 +0000

    add scripts to create fs force file

---
+++ phase/fs_4.0.1/world/create_force.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ $# == 4 ]]; then
+
+    #-- Set input and output environment variables
+    sids="$1"
+    prids="$2"
+    C1="$3"
+    C2="$4"
+    out="$(basename $sids)"
+
+    #-- Get the Ethnicities/Continents of the indivs into a file
+    grep -f $sids $prids | \
+        awk -v a="$C1" -v b="$C2" '{print $b}' | uniq > temp.eth
+    
+
+    #-- Extract the indivs and their corresponding eth/conts
+    grep -f $sids $prids | \
+    	awk -v a="$C1" -v b="$C2" '{print $a,$b}' > ${sids/.*/.eth}
+    
+
+    #-- Make the Force file
+    for i in $(cat temp.eth); do \
+        grep -wi $i ${sids/.*/.eth} | \
+            awk '{print $1}' | \
+            tr "\n" "," | \
+            sed 's/,$//' | \
+            awk -v pop="$i" 'OFS=""; {print pop,"(",$0,")"}'; 
+    done > ${out/.*/_force.txt}
+
+    rm temp.eth
+    
+    else 
+        echo """
+    	Usage:./create_force.sh <SIDs> <PRIDs>
+    	   
+    	     SIDs: Sample ID list for the current fs project (Single column)
+    	    PRIDs: Super list of all individual IDs plust their associated ethnicityes or continents
+    	       C1: Column number of the Individual IDs in the Super list
+               C2: Column number of the individual ethnicity or continent
+        """
+fi
