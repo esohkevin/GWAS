cf95172c0bf1b2a262a7d258d6b9e24410fd3d06
esohkevin <esohkevin4@gmail.com>
2019-10-21 16:06:08 +0200

    updates

--- popstruct/selection/vcf2vep.sh
+++ popstruct/selection/vcf2vep.sh
@@ -17,3 +17,9 @@ plink \
    --out ihsSignals \
    --real-ref-alleles \
    --thin-indiv-count 2
+
+cut -f1-20,22,26 ihs.hg19_multianno.txt | cut -f1-2,4-11,21- | sed 's/chr//g' | sed 's/\t/:/1' | sed 's/Chr:/key\t/' | sed 's/\t\t/\t/' > annv.txt
+
+Rscript signals.R
+
+awk '{print $2"\t"$1"\t"$10"\t"$11"\t"$5"\t"$8"\t"$9"\t"$13"\t"$12}' signals.table | head -1 > ihs.tbl.txt; awk '{print $2"\t"$1"\t"$10"\t"$11"\t"$5"\t"$8"\t"$9"\t"$13"\t"$12}' signals.table | grep -e exonic -e UTR | grep -v ncRNA_exonic | sort -n --key=5 >> ihs.tbl.txt
