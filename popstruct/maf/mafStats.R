3514e85e8f8d2faab093d12d157b34b7abca619e
esohkevin <esohkevin4@gmail.com>
2020-02-25 20:41:54 +0300

    update

--- popstruct/maf/mafStats.R
+++ popstruct/maf/mafStats.R
@@ -49,7 +49,8 @@ if (length(args) < 3 ) {
      pops <- unique(frq$CLST)
      print(paste0("# Populations: ", length(pops)), quote=F)
      n <- length(pops)
-     pcol <- RColorBrewer::brewer.pal(n, "Dark2")
+     #pcol <- RColorBrewer::brewer.pal(n, "Dark2")
+     pcol <- rainbow(n)
      for (popindex in 1:n) {
 	     frqbins[,pops[popindex]] <- c(length(unique(frq$SNP[frq$MAF < 0.1 & frq$CLST == pops[popindex]]))/length(unique(frq$SNP)),
                 length(unique(frq$SNP[frq$MAF >= 0.1 & frq$MAF < 0.2 & frq$CLST == pops[popindex]]))/length(unique(frq$SNP)),
@@ -81,16 +82,20 @@ if (length(args) < 3 ) {
 }
 
 #--- Quick plot
-#f <- read.table("maf.txt",h=T)
-#png("camMaf.png", height=15, width=15, units="cm", res=100, points=12)
-#plot(0, 0, xlim=c(0.1,1), ylim=c(0,0.5),
-#          type="n",xlab="MAF bin", ylab="Proportion of SNPs",
-#          main="Minor allele frequency spectrum")
-#plot(0,0,type="n", xlim=c(0,1), ylim=c(0,0.5))
-#pcol <- c("green","red","blue")
-#for (i in 2:ncol(f)) {
-#lines(data.frame(f$bin, f[i]), lty=1, lwd=1, col=pcol[i-1])
-#}
-#legend("topright", legend=c("BA","FO","SB"), col=c("green","red","blue"), lty=1, bty="n")
-#dev.off()
+f <- read.table("maf.txt",h=T)
+png("camMaf.png", height=10, width=10, units="cm", res=200, points=8)
+plot(0, 0, xlim=c(0,1), ylim=c(0,0.5),
+          type="n",xlab="MAF bin", ylab="Proportion of SNPs",
+          main="Minor allele frequency spectrum")
+pcol <- c("green","red","blue")
+for (i in 2:ncol(f)) {
+  if (i < ncol(f)) {
+    lines(data.frame(f$bin, f[i]), lty=1, lwd=1, col=pcol[i-1])
+  }
+  else {
+    lines(data.frame(f$bin, f[i]), lty=2, lwd=1, col=pcol[i-1])
+  }
+}
+legend("topright", legend=c("BA","FO","SB"), col=c("green","red","blue"), lty=c(1,1,2), bty="n")
+dev.off()
 
