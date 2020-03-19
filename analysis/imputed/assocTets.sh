0f9536d2828797e4e7287ab9d1c604e4a7d2ce8d
esohkevin <esohkevin4@gmail.com>
2020-03-19 20:54:07 +0200

    updates

--- analysis/imputed/assocTets.sh
+++ analysis/imputed/assocTets.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testbansemiban --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../samples/include_bantu-semibantu.txt
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testbansemiban --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../../samples/include_bantu-semibantu.txt
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testban --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../samples/include_bantu.txt
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testban --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../../samples/include_bantu.txt
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testsemiban --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../samples/include_semibantu.txt
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testsemiban --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../../samples/include_semibantu.txt
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testsm --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../samples/smcon.ids
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testsm --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../../samples/smcon.ids
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testsma --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../samples/smacon.ids
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testsma --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../../samples/smacon.ids
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testcm --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../samples/cmcon.ids
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testcm --covar phasedWrefImpute2.cov --covar-name C1-C10 --keep ../../samples/cmcon.ids
 
-plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testNofo --covar phasedWrefImpute2.cov --covar-name C1-C10 --remove ../samples/exclude_fo.txt
+plink --bfile imputed_clean --logistic --ci 0.95 --adjust --out testNofo --covar phasedWrefImpute2.cov --covar-name C1-C10 --remove ../../samples/exclude_fo.txt
 
 
